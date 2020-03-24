c063c7e7d745c43d07396a9f61e9f23cc36c0a09
Vazgen <vazgenmikaelyan82@gmail.com>
2020-03-24 19:01:05 +0400

    Add demo page

---
+++ precache-manifest.d672e27f5bbf355cd34919b5f01e8deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1cd5367c4d5df28b391e900e69fe67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5ad2e33f62929d0117",
+    "url": "/static/css/main.2381e901.chunk.css"
+  },
+  {
+    "revision": "cfe56cc26cbfeff96c0e",
+    "url": "/static/js/2.5b813e66.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5b813e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5ad2e33f62929d0117",
+    "url": "/static/js/main.fec4f890.chunk.js"
+  },
+  {
+    "revision": "f83478b7686e2421fcd4",
+    "url": "/static/js/runtime-main.05c7f52b.js"
+  }
+]);
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppComponent from './AppComponent';
-
-ReactDOM.render(
-    <AppComponent />,
-    document.getElementById('app-root')
-);
